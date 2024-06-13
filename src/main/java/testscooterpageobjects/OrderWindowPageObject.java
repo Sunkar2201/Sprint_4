f752bd77aaba11bad4d11499e31a851feabd4cc2
Sunkar22 <kussayinovsunkar@gmail.com>
2024-06-13 11:08:42 +0500

    modified WebDriver in OrderWindowPageObject

--- src/main/java/testscooterpageobjects/OrderWindowPageObject.java
+++ src/main/java/testscooterpageobjects/OrderWindowPageObject.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class OrderWindowPageObject {
-    private static WebDriver driver;
+    private WebDriver driver;
 
     public OrderWindowPageObject(WebDriver driver){
         this.driver = driver;
